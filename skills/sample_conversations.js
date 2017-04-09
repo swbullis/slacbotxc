45e8eea544677da79ef57b722f6fed2a429e2f81
Steve Bullis <steve@stevebullis.com>
2017-04-08 18:28:14 -0700

    Favorite Car?

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -22,6 +22,12 @@ module.exports = function(controller) {
                 convo.next();
 
             });
+            convo.ask('What is your favorite car?', function(response, convo) {
+
+                convo.say('Cool, I like ' + response.text + ' too!');
+                convo.next();
+
+            });
         });
 
     });
